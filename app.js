3b304045df2548f993b121354f327f61aeb54da5
Karol Programuje <karol341654@gmail.com>
2023-06-18 19:35:43 +0200

    code changes

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
-const bodyParser = require("body-parser");
 const express = require("express");
+const bodyParser = require("body-parser");
 const request = require("request");
 
 const app = express();
+app.use("/public", express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: "true"}));
 
 app.get("/", function(req, res) {
+
    res.sendFile(__dirname + "/signup.html");
 })
 
+app.post("/", function(req, res) {
+    var firstName = req.body.fName;
+    var lastName = req.body.lName;
+    var emailAddress = req.body.emailAddress;
 
+    console.log(firstName, lastName, emailAddress);
+    
+})
 app.listen(3000, function(req, res) {
     console.log("Server running on port 3000...");
 })
